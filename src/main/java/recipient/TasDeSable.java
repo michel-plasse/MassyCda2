ece0f20884afef130063c641ffa6bb9d1df5bc2d
Florian Langlois <florianlnglois@gmail.com>
2021-11-02 10:51:22 +0100

    correction de peutDeplacerTas et création des 2 tests pour peutDeplacerTas

--- src/main/java/recipient/TasDeSable.java
+++ src/main/java/recipient/TasDeSable.java
@@ -26,7 +26,7 @@ public class TasDeSable {
   }
   
   public boolean peutDeplacerTas(){
-      return (tasInitial.getQuantite()+brouette.getQuantite()+tasFinal.getQuantite())<=tasFinal.getCapacite();
+      return (tasInitial.getQuantite()+brouette.getQuantite())<=tasFinal.getEspaceLibre();
   }
   /** Déplace le sable du tas initial vers le tas final.
    * Requiert que la brouette soit vide au départ. Hypothèse à discuter,
@@ -37,13 +37,12 @@ public class TasDeSable {
    * Garantit que getTasInitial().estVide() et getBrouette().estVide().
    */
   void deplacerTas() {
-    assert peutDeplacerTas():"la capacite du tas final est insuffisante pour deplacer le tas";
+    assert peutDeplacerTas():"l'espace libre du tas final est insuffisant pour deplacer le tas";
     while (!tasInitial.estVide()) {
       while (!brouette.estPlein() && !tasInitial.estVide()) {
         tasInitial.mettreUnDans(brouette);
       }
       brouette.viderDans(tasFinal);
     }
-
   }
 }
--- src/test/java/recipient/TasDeSableTest.java
+++ src/test/java/recipient/TasDeSableTest.java
@@ -16,6 +16,26 @@ public class TasDeSableTest {
     assertTrue(tasInitial.estVide());
     assertTrue(brouette.estVide());
     assertEquals(9, tasFinal.getQuantite());
-  }
+    assertTrue(tasFinal.invariantEstOk());
+    assertTrue(tasInitial.invariantEstOk());
+    assertTrue(brouette.invariantEstOk());
 
+  }
+  
+  @Test
+  public void testPeutDeplacerTasOk(){
+    Recipient tasInitial = new Recipient(4, 4);
+    Recipient brouette = new Recipient(1, 3);
+    Recipient tasFinal = new Recipient(5,20);
+    TasDeSable instance = new TasDeSable(tasInitial, brouette, tasFinal);
+    assertTrue(instance.peutDeplacerTas());
+  }
+  @Test
+  public void testPeutDeplacerTasPasOk(){
+    Recipient tasInitial = new Recipient(4, 4);
+    Recipient brouette = new Recipient(1, 3);
+    Recipient tasFinal = new Recipient(5,7);
+    TasDeSable instance = new TasDeSable(tasInitial, brouette, tasFinal);
+    assertFalse(instance.peutDeplacerTas());
+  }
 }
