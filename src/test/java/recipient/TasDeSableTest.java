32bcc0f22d4af4e1a5adc0793c96e67623b2f931
HQRDa11 <thomasaserfroc@yahoo.fr>
2021-11-02 10:55:56 +0100

    Merge Dev
    Conflicts:
    	src/demo/Demo.java

--- src/test/java/recipient/TasDeSableTest.java
+++ src/test/java/recipient/TasDeSableTest.java
@@ -9,14 +9,15 @@ public class TasDeSableTest {
     }
 
     @Test
-    public void testDeplacerTas() {
+    public void testDeplacerTasOk() {
         // Given (initialisation): 
         Recipient tasInitial = new Recipient(5, 5);
         Recipient brouette = new Recipient(1, 2);
         Recipient tasFinal = new Recipient(3, 10);
 
-        TasDeSable instance = new TasDeSable(tasInitial, brouette, tasFinal);
-        assert instance.peutDeplacerTas() : "Le tas ne peut pas être déplacé";
+        TasDeSable instance;
+        instance = new TasDeSable(tasInitial, brouette, tasFinal);
+        assertTrue(instance.peutDeplacerTas());
 
         // Where:
         instance.deplacerTas();
@@ -25,5 +26,16 @@ public class TasDeSableTest {
         assertTrue(brouette.estVide());
         assertEquals(9, tasFinal.getQuantite());
     }
+    @Test
+    public void testDeplacerTasKo() {
+        // Given (initialisation): 
+        Recipient tasInitial = new Recipient(5, 5);
+        Recipient brouette = new Recipient(1, 2);
+        Recipient tasFinal = new Recipient(9, 10);
 
+        TasDeSable instance;
+        instance = new TasDeSable(tasInitial, brouette, tasFinal);
+        assertFalse(instance.peutDeplacerTas());
+;
+    }
 }
