5892ecbf724126e53380fc1a487ab192608f5fff
Sarah <ikescd@gmail.com>
2021-11-02 16:57:43 +0100

    add getTaille, getMinimum, getMaximum and ajouter on Echantillon

---
+++ src/main/java/genericite/Echantillon.java
@@ -0,0 +1,97 @@
+package genericite;
+
+import java.util.ArrayList;
+
+/**
+ * Echantillon de valeurs, pourvu d'une taille, un minimum, une moyenne, un
+ * maximum.
+ * <br>L'implémentation stocke le résumé des données, pas leur liste.
+ *
+ * @author michel
+ */
+public class Echantillon {
+    // Après l'instanciation, les nombres valent la valeur par défaut
+    // d'un nombre en Java, à savoir 0 (entiers) ou 0.0 (nombres décimaux).
+
+    private ArrayList<Double> valeurs;
+
+    public Echantillon() {
+        valeurs = new ArrayList<Double>();
+    }
+
+    /**
+     * Nombre d'éléments de l'échantillon
+     */
+    public int getTaille() {
+        return valeurs.size();
+    }
+
+    /**
+     * Valeur minimum. Requiert getTaille() > 0
+     */
+    public double getMinimum() {
+        // Le require
+        assert getTaille() > 0;
+        // Le code
+        double result = valeurs.get(0);
+
+        for (int i = 1; i < valeurs.size(); i++) {
+            if (valeurs.get(i) < result) {
+                result = valeurs.get(i);
+            }
+        }
+        return result;
+    }
+
+    /**
+     * Valeur moyenne. Requiert getTaille() > 0
+     */
+    public double getMoyenne() {
+        assert getTaille() > 0;
+        double sum = 0;
+        double moyenne = 0;
+        for (int i = 1; i < valeurs.size(); i++) {
+            sum += valeurs.get(i);
+        }
+        moyenne = sum / getTaille();
+
+        return moyenne;
+    }
+
+    /**
+     * Valeur maximum. Requiert getTaille() > 0
+     */
+    public double getMaximum() {
+        // Le require
+        assert getTaille() > 0;
+        // Le code
+        double result = valeurs.get(0);
+
+        for (int i = 1; i < valeurs.size(); i++) {
+            if (valeurs.get(i) > result) {
+                result = valeurs.get(i);
+            }
+        }
+        return result;
+    }
+
+    /**
+     * Ajouter une valeur à l'échantillon. Garantit que getTaille() est
+     * incrémentée de 1.
+     */
+    public void ajouter(double valeur) {
+        valeurs.add(valeur);
+        assert estInvariantOk();
+    }
+
+    /**
+     * Conditions toujours vraies
+     */
+    public boolean estInvariantOk() {
+        assert getTaille() >= 0 : "taille négative";
+        assert getTaille() == 0 || getMinimum() <= getMoyenne() : "minimum > moyenne";
+        assert getTaille() == 0 || getMoyenne() <= getMaximum() : "moyenne > maximum";
+        return true;
+    }
+
+}
--- src/test/java/demo/DemoTest.java
+++ src/test/java/demo/DemoTest.java
@@ -4,30 +4,36 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-
 public class DemoTest {
-  
-  @Test
-  public void test1() {
-    System.out.println("test1");
-  }
-  
-  @Test
-  public void test2() {
-    System.out.println("test2");
-  }
 
-  public void test3() {
-    System.out.println("test3");
-  }
-  
-  @BeforeEach
-  public void doBefore() {
-    System.out.println("\nbefore");
-  }
-  
-  @AfterEach
-  public void doAfter() {
-    System.out.println("after");
-  }
+    @Test
+    public void demoFor() {
+        for (int i = 0; i < 2; i++) {
+            System.out.println("test :  " + i);
+        }
+    }
+
+    @Test
+    public void test1() {
+        System.out.println("test1");
+    }
+
+    @Test
+    public void test2() {
+        System.out.println("test2");
+    }
+
+    public void test3() {
+        System.out.println("test3");
+    }
+
+    @BeforeEach
+    public void doBefore() {
+        System.out.println("\nbefore");
+    }
+
+    @AfterEach
+    public void doAfter() {
+        System.out.println("after");
+    }
 }
