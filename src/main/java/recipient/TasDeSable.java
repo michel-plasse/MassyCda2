0487387f28be895b58a846f84cf430a8a2921f27
Michel Plasse <mplasse@free.fr>
2021-11-02 11:16:48 +0100

    ajoute peutDeplacerTas() dans TasDeSable

--- src/main/java/recipient/TasDeSable.java
+++ src/main/java/recipient/TasDeSable.java
@@ -24,23 +24,32 @@ public class TasDeSable {
   public Recipient getTasFinal() {
     return tasFinal;
   }
-  
-  
-  /** Déplace le sable du tas initial vers le tas final.
-   * Requiert que la brouette soit vide au départ. Hypothèse à discuter,
-   * si nous la supprimons, il faut revoir le sens et les ocntrats de
-   * cette opération.
-   * Quel est le prérequis avec cette hypothèse, et quel serait-il sans
-   * elle ?
-   * Garantit que getTasInitial().estVide() et getBrouette().estVide().
+
+  /**
+   * Déplace le sable du tas initial vers le tas final. Requiert que la brouette
+   * soit vide au départ. Hypothèse à discuter, si nous la supprimons, il faut
+   * revoir le sens et les ocntrats de cette opération. Quel est le prérequis
+   * avec cette hypothèse, et quel serait-il sans elle ? Garantit que
+   * getTasInitial().estVide() et getBrouette().estVide().
    */
   void deplacerTas() {
+    assert peutDeplacerTas() : "Pas assez d'espace libre dans le tas final pour déplacer le tas initial";
     while (!tasInitial.estVide()) {
       while (!brouette.estPlein() && !tasInitial.estVide()) {
         tasInitial.mettreUnDans(brouette);
       }
       brouette.viderDans(tasFinal);
     }
+  }
 
+  /**
+   * Peut-on déplacer le tas initial dans le tas final ? Vrai quand
+   * getTasInitial().getQuantite() + getBrouette().getQuantite() &lt;
+   * getTasFinal().getEspaceLibre()
+   *
+   * @return
+   */
+  public boolean peutDeplacerTas() {
+    return tasInitial.getQuantite() + brouette.getQuantite() <= tasFinal.getEspaceLibre();
   }
 }
--- src/test/java/recipient/TasDeSableTest.java
+++ src/test/java/recipient/TasDeSableTest.java
@@ -18,4 +18,24 @@ public class TasDeSableTest {
     assertEquals(9, tasFinal.getQuantite());
   }
 
+  @Test
+  public void testPeutDeplacerTasOk() {
+    System.out.println("peutDeplacerTas");
+    Recipient tasInitial = new Recipient(5, 5);
+    Recipient brouette = new Recipient(1, 3);
+    Recipient tasFinal = new Recipient(0, 20);
+    TasDeSable instance = new TasDeSable(tasInitial, brouette, tasFinal);
+    assertTrue(instance.peutDeplacerTas());
+  }
+
+  @Test
+  public void testPeutDeplacerTasKo() {
+    System.out.println("peutDeplacerTas");
+    Recipient tasInitial = new Recipient(5, 5);
+    Recipient brouette = new Recipient(0, 3);
+    Recipient tasFinal = new Recipient(2, 5);
+    TasDeSable instance = new TasDeSable(tasInitial, brouette, tasFinal);
+    assertFalse(instance.peutDeplacerTas());
+  }
+
 }
