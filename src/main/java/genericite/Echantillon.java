a144d70fcd91638ecf1174c303ffa04878544cdb
kamal9401 <kamalknina@hotmail.fr>
2021-11-04 16:30:54 +0100

    first commit

--- src/main/java/genericite/Echantillon.java
+++ src/main/java/genericite/Echantillon.java
@@ -1,5 +1,7 @@
 package demo;
 
+import java.util.ArrayList;
+
 /**
  * Echantillon de valeurs, pourvu d'une taille, un minimum, une moyenne,
  * un maximum.
@@ -10,61 +12,87 @@ public class Echantillon {
   // Après l'instanciation, les nombres valent la valeur par défaut
   // d'un nombre en Java, à savoir 0 (entiers) ou 0.0 (nombres décimaux).
   private int taille;
-  private double minimum, somme, maximum;
+  private ArrayList<Double> valeurs;
 
+  public Echantillon(){
+      valeurs = new ArrayList<>();
+  }
   /**
    * Nombre d'éléments de l'échantillon
+   * @return  nb d'elements dans l'echantillon
    */
   public int getTaille() {
-    return taille;
+    return valeurs.size();
   }
 
   /**
    * Valeur minimum. Requiert getTaille() > 0
+   * @return la valeur minimum dans l'Echantillon
    */
   public double getMinimum() {
     // Le require
     assert taille > 0;
     // Le code
-    return minimum;
+    double result = valeurs.get(0);
+    for(int i=1; i< valeurs.size();i++)
+    {
+        if(valeurs.get(i)<result);
+        {
+            result = valeurs.get(i);
+        }
+    }
+    return result;
   }
 
   /**
    * Valeur moyenne. Requiert getTaille() > 0
+   * @return la moyenne des elements de l'echantillon
    */
   public double getMoyenne() {
+    // Le require
     assert taille > 0;
-    return somme / taille;
+    // Le code
+    double result=0;
+    for(int i =0; i< valeurs.size();i++)
+    {
+        result+=valeurs.get(i);
+    }
+    return result/valeurs.size();
   }
 
   /**
    * Valeur maximum. Requiert getTaille() > 0
+   * @return  la valeur maximum dans l'Echantillon
    */
   public double getMaximum() {
+    // Le require
     assert taille > 0;
-    return maximum;
+    // Le code
+    double result = valeurs.get(0);
+    for(int i=1; i< valeurs.size();i++)
+    {
+        if(valeurs.get(i)>result);
+        {
+            result = valeurs.get(i);
+        }
+    }
+    return result;
   }
 
   /**
    * Ajouter une valeur à l'échantillon. Garantit que getTaille() est
    * incrémentée de 1.
+   * @param valeur: valeur a ajouter
    */
   public void ajouter(double valeur) {
-    if (taille == 0) {
-      minimum = valeur;
-      maximum = valeur;
-    } else {
-      minimum = Math.min(minimum, valeur);
-      maximum = Math.max(maximum, valeur);
-    }
-    taille++;
-    somme += valeur;
+    valeurs.add(valeur);
     // les invariants doivent etre respectes
     assert estInvariantOk();
   }
 
   /**
    * Conditions toujours vraies
+   * @return true si les nvariants sont ok
    */
   public boolean estInvariantOk() {
     assert getTaille() >= 0 : "taille négative";
--- src/main/java/heritage/Commercial.java
+++ src/main/java/heritage/Commercial.java
@@ -29,5 +29,4 @@ public class Commercial extends Salarie{
         return super.getSalaire(); //To change body of generated methods, choose Tools | Templates.
     }
    
-    
-jjggk
+}
