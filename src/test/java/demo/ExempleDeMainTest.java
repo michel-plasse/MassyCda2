a568c88f95a21f7d446ab69fc6ecac4c8c04ec86
HQRDa11 <thomasaserfroc@yahoo.fr>
2021-11-08 09:44:04 +0100

    Create ExempleDeMainTest.java

---
+++ src/test/java/demo/ExempleDeMainTest.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit5TestClass.java to edit this template
+ */
+package demo;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ *
+ * @author thoma
+ */
+public class ExempleDeMainTest {
+    
+    public ExempleDeMainTest() {
+    }
+
+    @Test
+    public void testMain() {
+        System.out.println("main");
+        String[] args = null;
+        ExempleDeMain.main(args);
+        fail("The test case is a prototype.");
+    }
+    
+}
